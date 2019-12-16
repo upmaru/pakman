83e566e419cf175474407348d7d972b674b52cf4
Zack Siri <zacksiri@gmail.com>
2019-12-16 08:34:26 +0700

    fix bootstrap

--- lib/apkify/bootstrap.ex
+++ lib/apkify/bootstrap.ex
@@ -1,20 +1,25 @@
 defmodule Apkify.Bootstrap do
   @apkbuild "lib/apk/templates/apkbuild.eex"
+  @workspace System.get_env(~s(GITHUB_WORKSPACE))
 
   def perform(options) do
-    %{namespace: namespace, name: name} = Keyword.get(options, :repository)
+    [namespace, name] =
+      options
+      |> Keyword.get(options, :repository)
+      |> String.split("/")
+
     version = Keyword.get(options, :version)
     build = Keyword.get(options, :build)
 
     base_path = ".apk/#{namespace}/#{name}"
     File.mkdir_p(base_path)
 
-    [base_path, "APKBUILD"]
+    [@workspace, base_path, "APKBUILD"]
     |> Enum.join("/")
-    |> File.write(render_apkbuild(name, version, build))
+    |> File.write(render_apkbuild(@apkbuild, name, version, build))
   end
 
-  defp render_apkbuild(name, version, build) do
-    EEx.eval_file(@apkbuild, app_name: name, version: version, build: build)
+  defp render_template(file, name, version, build) do
+    EEx.eval_file(file, name: name, version: version, build: build)
   end
 end
