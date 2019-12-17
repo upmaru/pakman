62c524799626b5ff8fed88dfaae0b5070ac55401
Zack Siri <zacksiri@gmail.com>
2019-12-17 21:26:15 +0700

    added initd template

--- lib/apkify/bootstrap.ex
+++ lib/apkify/bootstrap.ex
@@ -17,6 +17,10 @@ defmodule Apkify.Bootstrap do
     base_path = ".apk/#{namespace}/#{name}"
     File.mkdir_p(base_path)
 
+    create_apkbuild(base_path, name, version, build, depends, makedepends)
+  end
+
+  defp create_apkbuild(base_path, name, version, build, depends, makedepends) do
     [@workspace, base_path, "APKBUILD"]
     |> Enum.join("/")
     |> File.write(Templates.apkbuild(name, version, build, depends, makedepends))
