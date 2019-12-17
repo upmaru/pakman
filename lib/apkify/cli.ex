bf60800151c0780768f878d909e2c2f59424bfb0
Zack Siri <zacksiri@gmail.com>
2019-12-17 21:03:49 +0700

    use github workspace env vars as root directory in APKBUILD

---
+++ lib/apkify/cli.ex
@@ -0,0 +1,25 @@
+defmodule Apkify.CLI do
+  alias Apkify.Bootstrap
+
+  def main(args \\ []) do
+    args
+    |> parse_args()
+    |> Bootstrap.perform()
+  end
+
+  defp parse_args(args) do
+    {opts, _word, _} =
+      args
+      |> OptionParser.parse(
+        switches: [
+          repository: :string,
+          version: :string,
+          build: :string,
+          depends: :string,
+          makedepends: :string
+        ]
+      )
+
+    opts
+  end
+end
