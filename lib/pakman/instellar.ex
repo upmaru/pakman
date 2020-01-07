5de33146d2c6d0d5ddbcb6310ada42a41dfa22d2
Zack Siri <zacksiri@gmail.com>
2020-01-07 22:02:12 +0700

    clean up multipart param

--- lib/pakman/instellar.ex
+++ lib/pakman/instellar.ex
@@ -17,9 +17,7 @@ defmodule Pakman.Instellar do
 
   def create_deployment(token, archive_path) do
     package_token = System.get_env("INSTELLAR_PACKAGE_TOKEN")
-    hash = System.get_env("GITHUB_SHA")
-    ref = System.get_env("GITHUB_REF")
-
+    
     headers = [
       {"authorization", "Bearer #{token}"},
       {"x-instellar-package-token", package_token}
@@ -29,8 +27,8 @@ defmodule Pakman.Instellar do
       Multipart.new()
       |> Multipart.add_content_type_param("application/x-www-form-urlencoded")
       |> Multipart.add_file(archive_path, name: "deployment[archive]")
-      |> Multipart.add_field("deployment[ref]", ref)
-      |> Multipart.add_field("deployment[hash]", hash)
+      |> Multipart.add_field("deployment[ref]", System.get_env("GITHUB_REF"))
+      |> Multipart.add_field("deployment[hash]", System.get_env("GITHUB_SHA"))
 
     post("/publish/deployments", multipart, headers: headers)
   end
