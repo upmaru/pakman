064d7fc056282b86bbd5b0af8a93c2a9e94e5442
Zack Siri <zacksiri@gmail.com>
2020-01-09 23:04:16 +0700

    remove castore

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Pakman.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :eex, :castore]
+      extra_applications: [:logger, :eex]
     ]
   end
 
