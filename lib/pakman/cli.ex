d4b457d30a855cb92078660a2e9f86d402f74a6a
Zack Siri <zacksiri@gmail.com>
2020-01-09 22:30:15 +0700

    fix option parser

--- lib/pakman/cli.ex
+++ lib/pakman/cli.ex
@@ -2,43 +2,20 @@ defmodule Pakman.CLI do
   @switches [
     switches: [
       command: :string,
-      options: :string
-    ]
-  ]
-
-  @sub_switches [
-    bootstrap: [
-      switches: [
-        version: :string,
-        build: :string,
-        depends: :string,
-        makedepends: :string
-      ]
-    ],
-    create_deployment: [
-      switches: [
-        package_path: :string
-      ]
-    ],
-    prepare_launch_deployment: [
-      switches: []
+      version: :string,
+      build: :string,
+      depends: :string,
+      makedepends: :string,
+      package_path: :string
     ]
   ]
 
   def main(args \\ []) do
-    {[command: command, options: options], _, _} = OptionParser.parse(args, @switches)
+    {[{:command, command} | options], _, _} =
+      OptionParser.parse(args, @switches)
+
     command = String.to_atom(command)
 
-    apply(Pakman, command, [parse_args(command, options)])
-  end
-
-  defp parse_args(command, args) do
-    switches = Keyword.fetch!(@sub_switches, command)
-
-    {opts, _word, _} =
-      args
-      |> OptionParser.parse(switches)
-
-    opts
+    apply(Pakman, command, options)
   end
 end
