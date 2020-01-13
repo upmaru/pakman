0eb914d67d4b89838ce900c2ec7f9f1a08fc193f
Zack Siri <zacksiri@gmail.com>
2020-01-13 21:39:39 +0700

    set permission

--- lib/pakman/deploy/prepare.ex
+++ lib/pakman/deploy/prepare.ex
@@ -3,10 +3,13 @@ defmodule Pakman.Deploy.Prepare do
   alias Pakman.Deploy.Templates
 
   def perform(_options) do
+    workspace = System.get_env("GITHUB_WORKSPACE")
     public_key = System.get_env("ABUILD_PUBLIC_KEY")
     package_token = System.get_env("INSTELLAR_PACKAGE_TOKEN")
     branch = Environment.branch()
     %{name: name} = Environment.repository()
+
+    System.cmd("sudo", ["chown", "-R", "builder:abuild", workspace])
 
     File.mkdir_p!(~s(deploy))
 
