64f108aa1acd261cfd1aafc1c51a99bc96dd1784
Zack Siri <me@zacksiri.dev>
2023-04-07 12:15:07 +0700

    ls base_path
    Signed-off-by: Zack Siri <me@zacksiri.dev>

--- lib/pakman/bootstrap.ex
+++ lib/pakman/bootstrap.ex
@@ -74,7 +74,7 @@ defmodule Pakman.Bootstrap do
     end
 
     Pakman.setup()
-    IO.inspect(File.ls!())
+    IO.inspect(File.ls!(base_path))
   end
 
   defp create_apkbuild(base_path, name, version, build, configuration) do
