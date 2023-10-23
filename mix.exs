4296672292939342a878ebf8c258f2f5ba3c13c5
Zack Siri <me@zacksiri.dev>
2023-10-23 16:36:39 +0700

    update action

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pakman.MixProject do
   def project do
     [
       app: :pakman,
-      version: "8.0.0",
+      version: "8.0.1",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
