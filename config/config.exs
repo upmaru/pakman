8c39f74119a98773e7cbc52c99da8b644912dbd5
Zack Siri <me@zacksiri.dev>
2023-10-26 20:34:04 +0700

    update adapter

--- config/config.exs
+++ config/config.exs
@@ -2,10 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 import Config
 
-config :tesla,
-       :adapter,
-       {Tesla.Adapter.Finch, name: Pakman.Finch, receive_timeout: 30_000}
-
 config :ex_aws,
   http_client: Pakman.ExAws.Client
 
