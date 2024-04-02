1d04256bd06c5a2eccf28434ec537a280f1c239e
Zack Siri <me@zacksiri.dev>
2024-04-02 08:20:12 +0700

    Use develop pakman

--- lib/pakman/setup.ex
+++ lib/pakman/setup.ex
@@ -3,6 +3,8 @@ defmodule Pakman.Setup do
 
   @system Application.compile_env(:pakman, :system) || System
 
+  @etc_apk_keys "/etc/apk/keys"
+
   def perform do
     home = System.get_env("HOME")
     private_key = System.get_env("ABUILD_PRIVATE_KEY")
@@ -28,6 +30,10 @@ defmodule Pakman.Setup do
     |> File.write!(private_key)
 
     abuild_config_path
+    |> Path.join("#{key_name}.rsa.pub")
+    |> File.write!(public_key)
+
+    @etc_apk_keys
     |> Path.join("#{key_name}.rsa.pub")
     |> File.write!(public_key)
   end
