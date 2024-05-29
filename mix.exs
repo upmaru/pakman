f1a6f9f561e5d82c9d5415259e10949fbbbc218c
Zack Siri <me@zacksiri.dev>
2024-05-29 15:31:29 +0700

    Bump to 8.2.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pakman.MixProject do
   def project do
     [
       app: :pakman,
-      version: "8.2.8",
+      version: "8.2.9",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
